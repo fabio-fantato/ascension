b3df691e7de9667f0cfb7c7f7ab7af68014cbffd
fabio-fantato <61248266+fabio-fantato@users.noreply.github.com>
2025-09-01 12:43:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// StackBlitz lida melhor com config em JS do que TS
 export default defineConfig({
   plugins: [react()],
-});
+})
